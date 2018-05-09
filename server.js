abb7670c59e2a152a9968abc236747128d4bd776
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-05-09 16:48:11 +0700

    Transfering file P2P using data channel

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/', function (req, res) {
 app.io.route('ready', function (req) {
     req.io.join(req.data.chat_room);
     req.io.join(req.data.signal_room);
+    req.io.join(req.data.files_room);
     app.io.room(req.data).broadcast('announce', {
         message: 'New client in the ' + req.data + 'room.'
     })
@@ -32,6 +33,13 @@ app.io.route('signal', function (req) {
     req.io.room(req.data.room).broadcast('signaling_message', {
         type: req.data.type,
         message: req.data.message
+    })
+})
+
+app.io.route('files', function (req) {
+    req.io.room(req.data.room).broadcast('files', {
+        filenime: req.data.filename,
+        filesize: req.data.filesize
     })
 })
 
